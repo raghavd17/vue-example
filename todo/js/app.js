edd16266f2182ee4705b4e673fe1872083540c54
Raghavendra S Diddimani <raghavendra.diddimani@ushamartintech.com>
2017-12-12 18:05:14 +0530

    todo app added

--- todo/js/app.js
+++ todo/js/app.js
@@ -1,40 +1,10 @@
-var list = new Vue({
+var app = new Vue({
   el: '#app',
   data: {
-    new_todo: '',
-    todos: JSON.parse(localStorage.getItem('todos'))
-  },
-  methods: {
-    add_newtask: function() {
-      var new_todo = this.new_todo
-      if (!new_todo == '' || !new_todo == null) {
-        this.todos.push({
-          task: new_todo,
-          completed: false
-        })
-        localStorage.setItem('todos', JSON.stringify(this.todos));
-        this.new_todo = ''
-        return
-      } else {
-        alert('add a task');
-      }
-    },
-    clear_taskentry: function() {
-        this.new_todo = ''
-        alert('hello');
-    },
-    task_done: function(ts_status) {
-
-      console.log(' before' + ts_status.completed);
-      this.ts_status = true;
-      console.log('After' + ts_status.completed);
-    }
-
+    // new_todo: '',
+    tasks:[
+      {name:'need to do this task'},
+      {name:'do pratice of vue js'}
+    ]
   }
-})
-
-//
-// localStorage.setItem('todos', JSON.stringify(todos));
-//
-// // Retrieve the object from storage
-// var retrievedObject = localStorage.getItem('todos');
+});
